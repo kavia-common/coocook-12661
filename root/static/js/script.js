9f4c000b995aee3ec89a99e0dbe3b5f433f552fa
Timon Foerster <timon@webfoerster.de>
2022-05-31 12:28:36 +0200

    (#144) add unsavedChanges.js to script.js

--- root/static/js/script.js
+++ root/static/js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 (() => {
 
     // reduce options of <select> inputs for unit to units applicable to selected article
@@ -85,3 +87,24 @@ function alertMessage(message, typ="success", dismissible=false, targetId="messa
 
     document.getElementById(targetId).append(alert);
 }
+
+const forms = document.getElementsByClassName('confirmIfUnsavedChanges');
+let hasUnsavedChanges = false;
+
+for( const form of forms ) {
+    const inputs =      Array.prototype.slice.apply(form.getElementsByTagName('input'));
+    const textareas =   Array.prototype.slice.apply(form.getElementsByTagName('textarea'));
+    const selects =     Array.prototype.slice.apply(form.getElementsByTagName('select'));
+    const elements =    inputs.concat(textareas).concat(selects);
+
+    for( const elem of elements ) {
+        elem.addEventListener('input', () => { if( !hasUnsavedChanges ) hasUnsavedChanges = true });
+    }
+
+    form.addEventListener('submit', () => { if( hasUnsavedChanges ) hasUnsavedChanges = false });
+}
+
+// See documentation about 'beforeunload' event on https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
+window.addEventListener('beforeunload', (e) => {
+    if( hasUnsavedChanges ) e.preventDefault(); //
+});
