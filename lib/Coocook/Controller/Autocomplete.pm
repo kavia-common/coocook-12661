3f44da5fa0ccc109e838509dee4214bf36e4f164
Daniel Böhmer <post@daniel-boehmer.de>
2022-06-21 22:12:07 +0200

    Provide JSON endpoint to autocomplete users (and organizations)

---
+++ lib/Coocook/Controller/Autocomplete.pm
@@ -0,0 +1,51 @@
+package Coocook::Controller::Autocomplete;
+
+use DateTime;
+use Moose;
+use MooseX::MarkAsMethods autoclean => 1;
+
+BEGIN { extends 'Coocook::Controller' }
+
+sub base : Chained('/base') PathPart('autocomplete') CaptureArgs(0) { }
+
+# TODO limit number of results
+# TODO sanitize input?
+# TODO split words in input
+# TODO deterministic sort order?
+# TODO rank direct matches first
+
+sub organizations_users : GET HEAD Chained('base') Args(0) Does('~Ajax')
+  RequiresCapability('autocomplete_users') {
+    my ( $self, $c ) = @_;
+
+    my $search = $c->req->params->get('search');
+
+    my $users = $c->model('Autocomplete')->organizations_users($search);
+
+    if ( not $users ) {
+        $c->response->status(400);
+        return;
+    }
+
+    $c->stash( json_data => $users );
+}
+
+sub users : GET HEAD Chained('base') Args(0) Does('~Ajax')
+  RequiresCapability('autocomplete_organizations') RequiresCapability('autocomplete_users') {
+    my ( $self, $c ) = @_;
+
+    my $search = $c->req->params->get('search');
+
+    my $users = $c->model('Autocomplete')->users($search);
+
+    if ( not $users ) {
+        $c->response->status(400);
+        return;
+    }
+
+    $c->stash( json_data => $users );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
