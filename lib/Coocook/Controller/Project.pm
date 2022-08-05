5b630ba86b0b5300bfea3c6629bab41a184439ab
Mose Schmiedel <mose@schmiednet.de>
2022-08-05 16:40:18 +0200

    Run perltidy on `Coocook::Controller::Project`

--- lib/Coocook/Controller/Project.pm
+++ lib/Coocook/Controller/Project.pm
@@ -176,7 +176,7 @@ sub edit : GET HEAD Chained('submenu') PathPart('edit') Args(0) RequiresCapabili
         meal_create_url      => $c->project_uri('/meal/create'),
     );
 
-    push @{ $c->stash->{js} }, '/js/project/edit.js';
+    push @{ $c->stash->{js} },  '/js/project/edit.js';
     push @{ $c->stash->{css} }, '/css/project/edit.css';
 
 }
