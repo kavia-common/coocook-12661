163dead8f151b03606c7f0e0f14ff5710eee1262
Daniel Böhmer <post@daniel-boehmer.de>
2022-11-08 16:56:31 +0100

    Delete Component::Schema::Convertible
    I am pretty sure convertible_into() isn't used anywhere and
    with the new units and unit_conversion system it's less
    trivial to write that method.

--- lib/Coocook/Schema/Result/Item.pm
+++ lib/Coocook/Schema/Result/Item.pm
@@ -7,8 +7,6 @@ use MooseX::MarkAsMethods autoclean => 1;
 
 extends 'Coocook::Schema::Result';
 
-__PACKAGE__->load_components('+Coocook::Schema::Component::Result::Convertible');
-
 __PACKAGE__->table('items');
 
 __PACKAGE__->add_columns(
