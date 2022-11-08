4092601c1757430afda9310923c1b526baf53a0a
Timon Foerster <timon@webfoerster.de>
2022-11-08 22:46:27 +0100

    remove old meals and dishes editor

--- lib/Coocook/Controller/Project.pm
+++ lib/Coocook/Controller/Project.pm
@@ -181,7 +181,6 @@ sub edit : GET HEAD Chained('submenu') PathPart('edit') Args(0) RequiresCapabili
         meal_create_url      => $c->project_uri('/meal/create'),
     );
 
-    push @{ $c->stash->{js} }, '/js/project/edit.js';
     push @{ $c->stash->{js} }, '/lib/web-js-components/meals-dishes-editor/meals-dishes-editor.es.js';
     push @{ $c->stash->{css} }, '/css/project/edit.css';
 }
