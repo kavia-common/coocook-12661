8e0c2df8235e06c0ff144ca83643f9bb5500e36b
Mose Schmiedel <mose@schmiednet.de>
2023-07-08 18:10:24 +0200

    Add script for managing coocooks external dependencies

---
+++ script/coocook_install_web_dependencies.pl
@@ -0,0 +1,225 @@
+#!/usr/bin/env perl
+
+# ABSTRACT: script for managing external dependencies downloaded from the internet
+
+use v5.24;
+use utf8;
+use warnings;
+use strict;
+
+use Cwd qw/abs_path getcwd/;
+use File::Basename;
+use File::Copy::Recursive qw/rmove/;
+use File::Fetch;
+use File::Path qw/rmtree make_path/;
+use Term::ANSIColor;
+use YAML::XS;
+
+my $dir = abs_path( getcwd() );
+if ( not( $dir =~ m{ ^ .* /coocook $ }x ) ) {
+    error('This script can only be executed in the top-level coocook repository');
+}
+
+my @dependencies =
+  map { Package->new($_) } @{ YAML::XS::LoadFile('web-dependencies.yaml')->{dependencies} };
+
+say colored( '=== downloading coocook web dependencies ===', 'cyan' );
+download( \@dependencies );
+say '';
+say colored( '=== installing coocook web dependencies ===', 'cyan' );
+install( \@dependencies );
+
+sub download {
+    my ($deps) = @_;
+
+    make_path '.cache';
+
+    for my $pkg ( $deps->@* ) {
+        $pkg->print_command('downloading');
+
+        # test if the package archive was already downloaded
+        if ( not( -e '.cache/' . $pkg->archive_name ) ) {
+            $pkg->download( dest_dir => '.cache' );
+            if ( not( -e '.cache/' . $pkg->archive_name ) ) {
+                $pkg->print_status('FAILED');
+            }
+            else {
+                $pkg->print_status('SUCCESS');
+            }
+        }
+        else {
+            $pkg->print_status('CACHED');
+        }
+    }
+}
+
+sub install {
+    my ($deps) = @_;
+
+    for my $pkg ( $deps->@* ) {
+        rmtree 'root/static/lib/' . $pkg->name;
+        make_path 'root/static/lib/' . $pkg->name;
+
+        $pkg->print_command('installing');
+        $pkg->unpack();
+        $pkg->print_status('SUCCESS');
+    }
+}
+
+package Package {
+    use Term::ANSIColor;
+
+    sub new {
+        my $class = shift;
+        my ($pkg_hash) = @_;
+        if ( not defined $pkg_hash->{extract_paths} ) {
+            $pkg_hash->{extract_paths} = [];
+        }
+        my $self = {
+            name          => $pkg_hash->{name},
+            url           => $pkg_hash->{url},
+            version       => $pkg_hash->{version},
+            extract_paths => [
+                map {
+                    $_->[0] =~ s/\$\{VERSION\}/$pkg_hash->{version}/g;
+                    $_->[1] =~ s/\$\{VERSION\}/$pkg_hash->{version}/g;
+                    $_
+                } $pkg_hash->{extract_paths}->@*
+            ],
+        };
+
+        # replace version placeholder with actual version number
+        $self->{url} =~ s/\$\{VERSION\}/$self->{version}/g;
+        $self->{ff} = File::Fetch->new( uri => $self->{url} );
+
+        return bless $self, $class;
+    }
+
+    sub name {
+        return shift->{name};
+    }
+
+    sub url {
+        return shift->{url};
+    }
+
+    sub version {
+        return shift->{version};
+    }
+
+    sub ff {
+        return shift->{ff};
+    }
+
+    sub extract_paths {
+        return shift->{extract_paths};
+    }
+
+    sub archive_name {
+        my $self = shift;
+        return $self->ff->output_file;
+    }
+
+    sub download {
+        my $self = shift;
+        my %args = @_;
+        $self->ff->fetch( to => $args{dest_dir} )
+          or error( $self->ff->error(1) );
+    }
+
+    sub print_command {
+        my $self = shift;
+        my ($command) = @_;
+
+        my $pkg_id         = $self->name . '@' . $self->version;
+        my $status_width   = length "SUCCESS";
+        my $terminal_width = `tput cols`;
+        my $command_width  = length "$command ";
+        my $right_margin   = 1;
+        my $status_margin  = 1;
+        my $points =
+          $terminal_width -
+          $right_margin -
+          $command_width -
+          $status_width -
+          $status_margin -
+          length "$pkg_id ";
+        print "$command $pkg_id ", '.' x $points;
+    }
+
+    sub print_status {
+        my $self   = shift;
+        my $status = shift;
+        if ( $status eq 'SUCCESS' ) {
+            say colored( " $status", 'green' );
+        }
+        elsif ( $status eq 'CACHED' ) {
+            say colored( "  $status", 'green' );
+        }
+        elsif ( $status eq 'FAILED' ) {
+            say colored( "  $status", 'red' );
+        }
+    }
+
+    sub unpack {
+        my $self         = shift;
+        my $archive_name = $self->archive_name;
+        my $tmp_dir      = File::Temp->newdir();
+
+        # compression formats supported by tar
+        # see https://en.wikipedia.org/wiki/Tar_(computing)#Suffixes_for_compressed_files
+        if ( $archive_name =~ /^ .* \. tar \. (bz2|gz|lz|lzma|lzo|xz|Z|zst) $/x ) {
+            my @tar_args = ( 'tar', '--extract', '--file' => ".cache/$archive_name" );
+            if ( $self->extract_paths->@* ) {
+                push @tar_args, ( '--directory' => $tmp_dir->dirname );
+                for my $mapping ( $self->extract_paths->@* ) {
+                    push @tar_args, "$mapping->[0]";
+                }
+            }
+            else {
+                push @tar_args, '--directory' => 'root/static/lib/' . $self->name;
+            }
+            system(@tar_args);
+        }
+        elsif ( $archive_name =~ /^ .* \. zip $/x ) {
+            my @unzip_args = ( 'unzip', '-q', ".cache/$archive_name" );
+            if ( $self->extract_paths->@* ) {
+                for my $mapping ( $self->extract_paths->@* ) {
+                    if ( $mapping->[0] =~ m{^.* / $}x ) {
+                        push @unzip_args, "$mapping->[0]*";
+                    }
+                    else {
+                        push @unzip_args, "$mapping->[0]";
+                    }
+                }
+                push @unzip_args, ( '-d' => $tmp_dir->dirname );
+            }
+            else {
+                push @unzip_args, ( '-d' => 'root/static/lib/' . $self->name );
+            }
+            system(@unzip_args);
+        }
+        else {
+            error("unsupported archive format: '$archive_name'");
+        }
+        if ( $self->extract_paths->@* ) {
+            for my $mapping ( $self->extract_paths->@* ) {
+                if ( $mapping->[1] ) {
+                    File::Copy::Recursive::rmove( $tmp_dir->dirname . "/$mapping->[0]",
+                        'root/static/lib/' . $self->name . "/$mapping->[1]" );
+                }
+            }
+        }
+    }
+}
+
+sub error {
+    my $msg              = shift;
+    my $cleanup_callback = shift;
+    say '';
+    say colored( $msg, 'red' );
+    if ( defined $cleanup_callback ) {
+        $cleanup_callback->();
+    }
+    exit 1;
+}
