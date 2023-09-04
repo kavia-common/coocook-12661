dboehmer/coocook

commit 44933296abc37f5c94bc1d941158d071dfe6c2c1

bootstrapification of login, register and recover

root/static/js/user/register.js
@@ -5,15 +5,17 @@
 
     const passwordElem  = document.querySelector('input[name="password"]');
     const password2Elem = document.querySelector('input[name="password2"]');
-    const meterElem =  document.createElement('span');
-    passwordElem.parentElement.appendChild(meterElem);
+    const meterElem =  document.getElementById('meter');
+
+    let html = 'strength: ' + '&#x2606;'.repeat( MAX_STARS );
+    meterElem.innerHTML = html;
 
     passwordElem.addEventListener('input', () => {
         let password = passwordElem.value;
 
         let stars = password == '' ? 0 : ( zxcvbn(password).score + 1 );
 
-        let html = 'strength: ';
+        html = 'strength: ';
         html += '&#x2605;'.repeat(             stars );
         html += '&#x2606;'.repeat( MAX_STARS - stars );
 
@@ -21,15 +23,14 @@
         meterElem.setAttribute( 'title', stars + ' of ' + MAX_STARS );
     });
 
-    const comparatorElem = document.createElement('span');
-    password2Elem.parentElement.appendChild(comparatorElem);
+    const comparatorElem = document.getElementById('comparator');
 
     [passwordElem, password2Elem].forEach(item => {
         item.addEventListener('input', () => {
             let password  = passwordElem.value;
             let password2 = password2Elem.value;
 
-            if( password.length || password2.length ) {
+            if( (password.length && password2.length) || (password2.length) ) {
                 comparatorElem.innerHTML = password == password2 ? "matches" : "doesn't match";
             }
             else {

root/static/css/style.css
@@ -228,8 +228,6 @@ li.checkbox {
     list-style-type: none;
 }
 
-
-
 form th {
     text-align: left;
 }
@@ -326,6 +324,10 @@ input[type=number].editable-content:not(:focus) {
 
 .hidden {
     display: none;
+}
+
+input:focus {
+    box-shadow: none !important;
 }
 
 /******** footer part ********/

root/templates/session/login.tt
@@ -1,19 +1,24 @@
-[% title = "Sign In" %]
+<div class="mx-auto" style="width:500px;">
+    <div class="content-box mb-3">
+        <div class="text-center mb-3">
+            <h2>Sign in</h2>
+        </div>
 
-[% IF register_url %]
-<p>You don’t have an account yet? <a href="[% register_url %]">Sign up now!</a></p>
-[% END %]
+        <form method="post" action="[% post_login_url %]" class="d-flex flex-column gap-3">
+            <input type="text"     name="username" placeholder="username" [% 'autofocus' IF NOT username %] value="[% username | html %]" class="form-control">
+            <input type="password" name="password" placeholder="password" [% 'autofocus' IF     username %] value="" class="form-control">
+            <div class="d-flex justify-content-between">
+                <div>
+                    <input id="store_username" class="form-check-input" type="checkbox" name="store_username" [% 'checked' IF store_username %]>
+                    <label for="store_username">Remember me</label>
+                </div>
+                <a href="[% recover_url %]">Lost your password?</a>
+            </div>
+            <input type="submit" value="Sign in" class="btn btn-primary">
+        </form>
+    </div>
 
-<form method="post" action="[% post_login_url %]">
-    <p>
-        <input type="text"     name="username" placeholder="username" [% 'autofocus' IF NOT username %] value="[% username | html %]">
-        <input type="password" name="password" placeholder="password" [% 'autofocus' IF     username %] value="">
-        <input type="submit" value="Sign in">
-    </p>
-    <p>
-        <input  id="store_username" type="checkbox" name="store_username" [% 'checked' IF store_username %]>
-        <label for="store_username">Permanently store username in browser</label>
-    </p>
-</form>
-
-<p><a href="[% recover_url %]">Lost your password?</a></p>
+    [% IF register_url %]
+        <p>You don’t have an account yet? <a href="[% register_url %]">Sign up now!</a></p>
+    [% END %]
+</div>

root/templates/user/recover.tt
@@ -1,9 +1,15 @@
-[% title = "Password recovery" %]
+<div class="mx-auto" style="width:500px;">
+    <div class="content-box mb-3">
+        <div class="text-center mb-3">
+            <h2>Password recovery</h2>
+        </div>
 
-<p>You can request a password recovery link to your email address.
-When you click it you can set a new password for your account.</p>
+        <p>You can request a password recovery link to your email address.
+        When you click it you can set a new password for your account.</p>
 
-<form method="post" action="[% recover_url %]">
-    <input type="email" name="email" value="[% email | html %]" placeholder="email address">
-    <input type="submit" value="Send recovery link">
-</form>
+        <form method="post" action="[% recover_url %]" class="d-flex flex-column gap-3">
+            <input type="email" name="email" value="[% email | html %]" placeholder="email address" class="form-control">
+            <input type="submit" value="Send recovery link" class="btn btn-primary">
+        </form>
+    </div>
+</div>

root/templates/user/register.tt
@@ -1,45 +1,37 @@
-[% title = "Sign Up" %]
+<div class="mx-auto" style="width:500px;">
+    <div class="content-box mb-3">
+        <div class="text-center mb-3">
+            <h2>Sign up</h2>
+        </div>
 
-<p><a href="[% login_url %]">Already have an account? Sign in here.</a></p>
-
-<form method="post" action="[% post_register_url %]">
-<table class="register">
-    <tr class="username">
-        <th><label for="username">Username</label></th>
-        <td>
-            <input id="username" type="text" name="username" placeholder="username" pattern="[A-Za-z0-9_]+" value="[% last_input.username | html %]" [% 'autofocus' IF NOT last_input.username %]>
-            [% IF example_username %]e.g. <code>[% example_username | html %]</code>[% END %]
-        </td>
-    </tr>
-    <tr class="email">
-        <th><label for="email">Email address</label></th>
-        <td><input  id="email" type="email" name="email" placeholder="email address" value="[% last_input.email | html %]" [% 'autofocus' IF last_input.username AND NOT last_input.email %]></td>
-    </tr>
-[%~ IF use_hidden_input %]
-    <tr class="url">
-        <th><label for="url">Website</label></th>
-        <td><input  id="url" type="url" name="url" placeholder="address of your website" value="[%# THIS INPUT FIELD ONLY WORKS AS CAPTCHA! %]" ></td>
-    </tr>
-[%~ END %]
-    <tr class="password">
-        <th><label for="password">Password</label></th>
-        <td><input  id="password"  type="password" placeholder="password" name="password" [% 'autofocus' IF last_input.username AND last_input.email %]></td>
-    </tr>
-    <tr class="password2">
-        <th><label for="password2">Confirm password</label></th>
-        <td><input  id="password2" type="password" placeholder="password" name="password2"></td>
-    </tr>
-</table>
-
-[% label = "Sign up";
-
-IF terms %]
-<input type="hidden" name="accept_terms" value="[% terms.id %]">
-
-<p>By clicking <em>[% label %]</em> you accept our <a href="[% terms.url %]">terms as of [% display_date(terms.valid_from, {html=>1}) %]</a>.</p>
+        <form method="post" action="[% post_register_url %]" class="d-flex flex-column gap-3">
+            <div>
+                <input id="username" type="text" name="username" placeholder="username" pattern="[A-Za-z0-9_]+" value="[% last_input.username | html %]" [% 'autofocus' IF NOT last_input.username %] class="form-control">
+[% IF example_username %]
+                e.g. <i>[% example_username | html %]</i>
 [% END %]
+            </div>
+            <input id="email" type="email" name="email" placeholder="email address" value="[% last_input.email | html %]" [% 'autofocus' IF last_input.username AND NOT last_input.email %] class="form-control">         
+[%~ IF use_hidden_input %]
+            <input id="url" type="url" name="url" placeholder="address of your website" value="[%# THIS INPUT FIELD ONLY WORKS AS CAPTCHA! %]" class="form-control">
+[%~ END %]
+            <div>
+                <input id="password"  type="password" placeholder="password" name="password" [% 'autofocus' IF last_input.username AND last_input.email %] class="form-control">
+                <span id="meter"></span>
+            </div>
+            <div>
+                <input id="password2" type="password" placeholder="confirm password" name="password2" class="form-control">
+                <span id="comparator"></span>
+            </div>
+[% label = "Sign up";
+IF terms %]
+            <input type="hidden" name="accept_terms" value="[% terms.id %]">
+            <span>By clicking <em>[% label %]</em> you accept our <a href="[% terms.url %]">terms as of [% display_date(terms.valid_from, {html=>1}) %]</a>.</span>
+[% END %]
+            <span>You will receive an email with a web link to verify your email address.</span>
+            <input type="submit" value="[% label %]" class="btn btn-primary">
+        </form>
+    </div>
 
-<p>You will receive an email with a web link to verify your email address.</p>
-
-<input type="submit" value="[% label %]">
-</form>
+    <p>Already have an account? <a href="[% login_url %]">Sign in here.</a></p>
+</div>

kavia-common/coocook-12661

commit 1f9abe1474c2b027707db4d295458f608a0556f3

Fix broken password strength meter on /settings/account
Broken in 44933296abc37f5c94bc1d941158d071dfe6c2c1.

- add <span> elements expected by changed JS code

- make JS always insert NBSP to avoid line collapsing

- same styling as on /user/register

root/static/js/user/register.js
@@ -1,3 +1,4 @@
+// !!! this file is also loaded on /settings/account for password change !!!
 (() => {
     document.querySelector('input[name="url"]')?.setAttribute('tabindex', -1);
 
@@ -5,10 +6,12 @@
 
     const passwordElem  = document.querySelector('input[name="password"]');
     const password2Elem = document.querySelector('input[name="password2"]');
-    const meterElem =  document.getElementById('meter');
+    const meterElem      = document.getElementById('meter');
+    const comparatorElem = document.getElementById('comparator');
 
     let html = 'strength: ' + '&#x2606;'.repeat( MAX_STARS );
     meterElem.innerHTML = html;
+    comparatorElem.innerHTML = '&nbsp;';
 
     passwordElem.addEventListener('input', () => {
         let password = passwordElem.value;
@@ -23,8 +26,6 @@
         meterElem.setAttribute( 'title', stars + ' of ' + MAX_STARS );
     });
 
-    const comparatorElem = document.getElementById('comparator');
-
     [passwordElem, password2Elem].forEach(item => {
         item.addEventListener('input', () => {
             let password  = passwordElem.value;
@@ -34,7 +35,7 @@
                 comparatorElem.innerHTML = password == password2 ? "matches" : "doesn't match";
             }
             else {
-                comparatorElem.innerHTML = '';
+                comparatorElem.innerHTML = '&nbsp;';
             }
         });
     });

root/templates/settings/account.tt
@@ -49,8 +49,17 @@
     <form method="post" action="[% change_password_url %]">
         <div class="mb-3" style="max-width: 300px;">
         <input type="password" name="current_password"  placeholder="current password" class="form-control mb-1">
+        </div>
+
+        <div class="mb-3" style="max-width: 300px;">
         <input type="password" name="password"  placeholder="new password" class="form-control mb-1">
+        <span id="meter"></span>
+        </div>
+
+        <div class="mb-3" style="max-width: 300px;">
         <input type="password" name="password2" placeholder="confirm new password" class="form-control">
+        <span id="comparator"></span>
+
         </div>
         <input type="submit" value="Change password" class="btn btn-primary mb-3">
     </form>